763b4608d83da576cafe05026d06a62e1f61cadc
parasrah <dev@parasrah.com>
2024-02-28 19:54:19 -0700

    fix conflict with competing animation modules

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,22 @@
 const MODULE_KEY = "alt-drag";
 const OPTION_KEY = "preventAnimation";
 
-const state = {
-  active: false,
-};
+const IS_KEY_HELD_OPT = "isKeyHeld";
+
+const active = () => game.settings.get(MODULE_KEY, IS_KEY_HELD_OPT);
+const setActive = (value) =>
+  game.settings.set(MODULE_KEY, IS_KEY_HELD_OPT, value);
+
+function registerSettings() {
+  game.settings.register(MODULE_KEY, IS_KEY_HELD_OPT, {
+    name: "Prevent Token Animation",
+    hint: "Prevent animating token drag",
+    scope: "client",
+    config: false,
+    type: Boolean,
+    default: false,
+  });
+}
 
 function registerKeybindings() {
   game.keybindings.register(MODULE_KEY, "moveWithoutAnimation", {
@@ -16,39 +29,32 @@ function registerKeybindings() {
         key: "AltLeft",
       },
     ],
-    precedence: -1,
+    precedence: CONST.KEYBINDING_PRECEDENCE.NORMAL,
   });
+}
+
+function registerWrappers() {
+  libWrapper.register(
+    MODULE_KEY,
+    `Token.prototype._onDragLeftDrop`,
+    onDragLeftDrop,
+    "MIXED",
+  );
 }
 
 function handleMoveWithoutAnimation(event) {
   const isKeyPressed = !event.up;
   const isGM = game.user.isGM;
-  state.active = isKeyPressed && isGM;
+  setActive(isKeyPressed && isGM);
 }
 
 Hooks.once("init", function () {
+  registerSettings();
   registerKeybindings();
 });
 
-Hooks.on("preUpdateToken", function (_document, changes, options, userId) {
-  const moveKeys = ["x", "y"];
-
-  const modActive = state.active;
-  const isMove = moveKeys.every((key) => changes.hasOwnProperty(key));
-  const isAlreadySet = options.hasOwnProperty(OPTION_KEY);
-
-  if (modActive && isMove && !isAlreadySet) {
-    options[OPTION_KEY] = true;
-  }
-});
-
-Hooks.on("updateToken", function (document, change, options, userId) {
-  if (OPTION_KEY in options && options[OPTION_KEY] === true) {
-    const token = document._object;
-
-    if (token && token._animation) {
-      const animation = CanvasAnimation.getAnimation(token.animationName);
-      animation.time = animation.duration;
-    }
+Hooks.on("preUpdateToken", function (document, changes, options, _userId) {
+  if (active()) {
+    options.animate = false;
   }
 });
